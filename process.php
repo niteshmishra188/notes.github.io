9152c12cfd01917a0f05c6b94726eb6ddf65e618
Nitesh Kumar Mishra <127732043+niteshmishra188@users.noreply.github.com>
2023-08-19 09:49:09 +0530

    My notes
    Add your note.

---
+++ process.php
@@ -0,0 +1,60 @@
+<?php
+
+$insert = false;
+$update = false;
+$delete = false;
+// Connect to the Database 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "notes";
+
+// Create a connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Die if connection was not successful
+if (!$conn){
+    die("Sorry we failed to connect: ". mysqli_connect_error());
+}
+
+if(isset($_GET['delete'])){
+  $sno = $_GET['delete'];
+  $delete = true;
+  $sql = "DELETE FROM `notes` WHERE `srn` = $sno";
+  $result = mysqli_query($conn, $sql);
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+if (isset( $_POST['snoEdit'])){
+  // Update the record
+    $sno = $_POST["snoEdit"];
+    $title = $_POST["titleEdit"];
+    $description = $_POST["descriptionEdit"];
+
+  // Sql query to be executed
+  $sql = "UPDATE `notes` SET `title` = '$title' , `description` = '$description' WHERE `notes`.`srn` = $sno";
+  $result = mysqli_query($conn, $sql);
+  if($result){
+    $update = true;
+}
+else{
+    echo "We could not update the record successfully";
+}
+}
+else{
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+
+  // Sql query to be executed
+  $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
+  $result = mysqli_query($conn, $sql);
+
+   
+  if($result){ 
+      $insert = true;
+  }
+  else{
+      echo "The record was not inserted successfully because of this error ---> ". mysqli_error($conn);
+  } 
+}
+}
+?>
